1de8d6b352cf1a8a1f8bdbe2ffce5e8c5a1cabb6
Arib Dhuka <atxarib99@gmail.com>
2022-07-02 15:35:53 -0500

    added utility function to created named output

--- monitor.py
+++ monitor.py
@@ -5,3 +5,15 @@ class Monitor(ABC):
     @abstractmethod
     def get(self):
         pass
+
+    def buildNamedOutput(data, names):
+        if len(data) != len(names):
+            return 'ERROR: buildNamedOutput(): length of data and names are not the same'
+        toreturn = "{"
+        for i in range(len(data)):
+            toreturn += names[i] + ':' + str(data[i])
+            if i < len(data) -1:
+                toreturn += ','
+        toreturn += '}'
+        #encapsulating the return in a list prevents daemon from splitting string into another tuple
+        return [toreturn]
