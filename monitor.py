37f4f2901462785c2d97c5161344cf554fecffa2
Arib Dhuka <atxarib99@gmail.com>
2022-07-07 11:38:49 -0500

    monitor now has function to get previous data

--- monitor.py
+++ monitor.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import time
+import os
 
 class Monitor(ABC):
 
@@ -22,4 +23,39 @@ class Monitor(ABC):
         #encapsulating the return in a list prevents daemon from splitting string into another tuple
         return [toreturn]
 
+    def getData(self):
+        with open(os.path.expanduser('~') + '/.guiltyspark/' + str(self.name) + '.dat', 'r') as f:
+            data = []
+            datanames = []
+            for line in f.readlines():
+                datapoint = line
+                #its a dict, format this data point, and get datanames
+                if '{' in datapoint:
+                    datapoint = datapoint.strip('{}\n').split(',')
+                    newdatapoint = []
+                    for point in datapoint:
+                        #populate datanames if not already done
+                        datanames.append(point.split(':')[0])
+                        newdatapoint.append(point.split(':')[1])
+                    datapoint = newdatapoint
+                #else its a single value
+                else:
+                    datapoint = [datapoint]
+                    datanames.append(self.name)
 
+                if data == []:
+                    for i in range(len(datapoint)):
+                        data.append([])
+
+                #build data list
+                for i in range(len(datapoint)):
+                    #handle %
+                    try:
+                        data[i].append(float(datapoint[i]))
+                    except:
+                        #try stripping '%' and try again
+                        try:
+                            data[i].append(float(datapoint[i].strip('%')))
+                        except:
+                            data[i].append(0)
+            return data, datanames
