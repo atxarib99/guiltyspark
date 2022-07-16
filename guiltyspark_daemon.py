20b3291874bc5310352b5817a0bb3c0d223c469c
Arib Dhuka <atxarib99@gmail.com>
2022-07-16 03:58:58 -0500

    added installation reference to daemon

--- guiltyspark_daemon.py
+++ guiltyspark_daemon.py
@@ -13,7 +13,8 @@ base = os.path.expanduser('~') + '/.guiltyspark/'
 #create dir if it doesn't exist
 if not os.path.exists(base):
     os.mkdir(base)
-    
+
+installPath = '/usr/local/guiltyspark/'
 
 import time
 
@@ -25,7 +26,7 @@ while True:
     #doesnt allow for variables. Monitor will need to be serializable. TODO: create example.
     monitors = []
     #get all monitors in the modules path
-    for item in os.listdir('./modules'):
+    for item in os.listdir(installPath + '/modules'):
         if item[-3:] == '.py':
             print(item)
             monitors.append(importlib.import_module('modules.' + item[0:-3], item[0:-3]).MonitorImpl())
