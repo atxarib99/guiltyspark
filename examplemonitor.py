7c4dfb4b120bfbdfed4ba720d26cba907eeed78f
Arib Dhuka <atxarib99@gmail.com>
2022-07-16 03:50:46 -0500

    added a command that brings the user a monitor template

---
+++ examplemonitor.py
@@ -0,0 +1,15 @@
+from monitor import Monitor
+
+import subprocess
+import json
+
+class MonitorImpl(Monitor):
+
+    name = 'examplemonitor'
+
+    def get(self):
+        #run code here
+
+
+        #return a value
+        return 0
